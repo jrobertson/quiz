b858bc50390633b867937633ed5e4a202332baa8
Your Name <james@r0bertson.co.uk>
2012-02-10 14:06:56 +0000

    now includes a block and score method

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -15,28 +15,35 @@ class Quiz
   def start(n=3)
 
     nodes = XPath.match(@doc.root, '*[question]')
-    list = (0..nodes.length).to_a.sample(n)
+    list = (0...nodes.length).to_a.sample(n)
     questions = nodes.values_at(*list)
 
-    results = questions.map do |q|
+    @results = questions.map do |q|
       question, answer = q.text('question').strip, q.text('answer').strip
       *options = XPath.match(q,'./options//option/text()').sort{rand}
       a_options = options.length.times.map {|x| (97 + x).chr }.\
         zip(options.map {|x| x.to_s.strip})
       q_options = a_options.map {|x| " " * 2 + "%s) %s" % x}
-      h = Hash[a_options.to_a] # answers are in the hash, keys = a,b,c,d
-      puts question + "\nis it ... \n" + q_options.join("\n")
+      answers = Hash[a_options.to_a] # answers are in the hash, keys = a,b,c,d
       
-      student_answer = gets.chop; redo unless h.keys.include? student_answer
-      
-      result = h[student_answer] == answer
-      reply = result ? '* correct *' : 'the answer was ' + answer
-      puts reply
+      if block_given? then
+        result = yield(question, q_options, answers, answer)
+      else
+        puts question + "\nis it ... \n" + q_options.join("\n")
+        
+        student_answer = gets.chop; redo unless answers.keys.include? student_answer
+        
+        result = answers[student_answer] == answer
+        reply = result ? '* correct *' : 'the answer was ' + answer
+        puts reply
+      end
       result
     end
-
-    tally = (results - [false]).size
-    percent = 100 / (results.size / tally.to_f)
+  end
+  
+  def score()
+    tally = (@results - [false]).size
+    percent = 100 / (@results.size / tally.to_f)
     your_score = "you scored %s%" % percent.to_i
 
   end
